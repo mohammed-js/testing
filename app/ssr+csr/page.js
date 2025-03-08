066f1cd88ec147c28bbc92727535cd8d50d6e196
Mohammed Moustafa <mohammedmoustafa.js@gmail.com>
2025-03-08 06:57:32 +0200

    2nd commit

--- app/ssr+csr/page.js
+++ app/ssr+csr/page.js
@@ -1,8 +1,8 @@
 // server component containing (server and client component) // server components can render client components, bun not vice versa (work around) // SC: can'y use hooks | CC: can't use ssr fetching
 // server component can be for components or whole page
 
-import ClientComponent from "../components/clientComponent";
-import ServerComponent from "../components/serverComponent";
+import ClientComponent from "../components/ClientComponent";
+import ServerComponent from "../components/ServerComponent";
 
 export default async function DashboardPage() {
   return (
